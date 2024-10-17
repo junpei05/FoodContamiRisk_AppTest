68968dd3e26957898b6b200a61f2abe47500dc50
Junpei Hosoe <86196108+junpei05@users.noreply.github.com>
2024-10-17 14:49:00 +0900

    Add files via upload

--- risk_concentration_test_JH.py
+++ risk_concentration_test_JH.py
@@ -9,11 +9,6 @@ import os
 # Streamlit CloudのSecretsで保存したテーマを取得
 theme_setting = os.getenv("THEME", "light")  # "light" or "dark"
 
-if theme_setting == "dark":
-    st.set_page_config(page_title="My App", layout="centered", theme="dark")
-else:
-    st.set_page_config(page_title="My App", layout="centered", theme="light")
-
 # ページの設定
 st.set_page_config(layout="wide", initial_sidebar_state="expanded")
 
